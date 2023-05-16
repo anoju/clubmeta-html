ce061b81642db586c38b0d9f1462a0f592772f4a
anoju <zun5761@gmail.com>
2023-05-16 15:36:16 +0900

    to-be

--- to-be/html/include/head.js
+++ to-be/html/include/head.js
@@ -4,8 +4,10 @@
   str += '<meta property="og:image" content="https://dev-mydata.mykkl.com/mydata/resources/static/images/og_image_1200x630.png" />';
   str += '<link href="../../static/css/front.min.css" rel="stylesheet" />';
   str += '<script type="text/javascript" src="../../static/js/lib/jquery-3.7.0.min.js"></script>';
+  str += '<script type="text/javascript" src="../../static/js/lib/jquery-ui.1.13.2.min.js"></script>';
   str += '<script type="text/javascript" src="../../static/js/lib/lodash.4.17.21.min.js"></script>';
   str += '<script type="text/javascript" src="../../static/js/ui-plugins.min.js"></script>';
+  str += '<script type="text/javascript" src="../../static/js/ui-util.js"></script>';
   str += '<script type="text/javascript" src="../../static/js/ui-front.js"></script>';
   document.write(str);
   const $include = document.querySelector('.__include');
